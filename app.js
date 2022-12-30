21551098911fe6c46855c13d830a38e37296ec28
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2022-12-30 12:09:48 +0100

    javascript to accordion menu

---
+++ app.js
@@ -0,0 +1,13 @@
+let elementosAcordeon = document.getElementsByClassName("accordion");
+
+for (let i = 0; i< elementosAcordeon.length; i++){
+    elementosAcordeon[1].addEventListener("click",function(){
+        this.classList.toggle("active");
+        let content = this.nextElementSibling();
+        if(content.style.display == "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    })
+}
