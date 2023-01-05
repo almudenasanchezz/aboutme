afe3429ebe3451d0c10e0cabd0eae8611f485e8e
almudenasanchezz <sanchezgallegoalmudena@gmail.com>
2023-01-05 16:31:25 +0100

    menú accordeon segun altura de contenido y botón +

--- js/app.js
+++ js/app.js
@@ -3,7 +3,14 @@ let elementosAcordeon = document.querySelectorAll("button[data-accordion-section
 elementosAcordeon.forEach(accordionButtonNode => {
   accordionButtonNode.addEventListener('click', function() {
     const accordionSection = this.getAttribute('data-accordion-section-ref');
-    const element = document.querySelector(`[data-accordion-section=${accordionSection}]`)
-    element.classList.toggle('accordion-visible');    
+    const contentNodeWrapper = document.querySelector(`[data-accordion-section=${accordionSection}]`)
+    const contentNode = contentNodeWrapper.children[0];
+    if (contentNodeWrapper.style.maxHeight) {
+      contentNodeWrapper.style.maxHeight = null;
+    } else {
+      contentNodeWrapper.style.maxHeight = `${contentNode.clientHeight+ 300}px`;
+    }
   });
 })
+
+
